f4ff8273eeec6afdfbc8b07cd40809ddf745e2d7
Miguel Criado <macro.masek@gmail.com>
2020-02-05 17:43:23 +0100

    Add Basic Tests for UIFrame

--- Tests/Runtime/UIFrameTests.cs
+++ Tests/Runtime/UIFrameTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.TestTools;
 
 namespace Muui.Tests
@@ -25,9 +26,47 @@ namespace Muui.Tests
 		}
 
 		[UnityTest]
-		public IEnumerator UIFrameObjectIsCreated()
+		public IEnumerator _00UIFrameObjectIsCreated()
 		{
-			Assert.True(uiFrame != null);
+			Assert.NotNull(uiFrame);
+			yield return null;
+		}
+
+		[UnityTest]
+		public IEnumerator _01UIFrameMainCanvasIsPresent()
+		{
+			Assert.NotNull(uiFrame.MainCanvas);
+			yield return null;
+		}
+
+		[UnityTest]
+		public IEnumerator _02UIFrameCameraIsPresent()
+		{
+			Assert.NotNull(uiFrame.UICamera);
+			yield return null;
+		}
+
+		[UnityTest]
+		public IEnumerator _03UIFrameEventSystemIsPresent()
+		{
+			EventSystem eventSystem = uiFrame.GetComponentInChildren<EventSystem>();
+			Assert.NotNull(eventSystem);
+			yield return null;
+		}
+
+		[UnityTest]
+		public IEnumerator _04UIFrameWindowLayerIsPresent()
+		{
+			WindowLayer windowLayer = uiFrame.GetComponentInChildren<WindowLayer>();
+			Assert.NotNull(windowLayer);
+			yield return null;
+		}
+
+		[UnityTest]
+		public IEnumerator _05UIFramePanelLayerIsPresent()
+		{
+			PanelLayer panelLayer = uiFrame.GetComponentInChildren<PanelLayer>();
+			Assert.NotNull(panelLayer);
 			yield return null;
 		}
 	}
