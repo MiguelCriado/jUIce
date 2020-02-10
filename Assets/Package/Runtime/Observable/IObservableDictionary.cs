f831c895befea9118e4ce0adf03b65e05d9e1eba
Miguel Criado <macro.masek@gmail.com>
2020-02-10 13:26:51 +0100

    Add ObservableDictionary

---
+++ Assets/Package/Runtime/Observable/IObservableDictionary.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Muui
+{
+	public interface IObservableDictionary<TKey, TValue> : IReadOnlyObservableDictionary<TKey, TValue>, IDictionary<TKey, TValue>
+	{
+
+	}
+}
