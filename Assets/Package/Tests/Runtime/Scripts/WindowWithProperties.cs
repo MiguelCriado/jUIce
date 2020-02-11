4ea7c8e17dd7278ff135aebc9743cf3bb98471c6
Miguel Criado <macro.masek@gmail.com>
2020-02-11 18:19:13 +0100

    Add Some Test for Template Screens

---
+++ Assets/Package/Tests/Runtime/Scripts/WindowWithProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Muui.Tests
+{
+	public class WindowWithProperties : BaseWindowController<WindowWithProperties.Properties>
+	{
+		[Serializable]
+		public class Properties : WindowProperties
+		{
+			public IObservableVariable<int> MyIntVariable;
+		}
+	}
+}
