54bee0ea0fb72ba213857a3aad4b6dcc8f356255
Miguel Criado <macro.masek@gmail.com>
2020-04-22 20:18:52 +0200

    Add PresenterInitializer

---
+++ Assets/Plugins/Muui/Scripts/Runtime/Presenter/PresenterInitializer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Muui
+{
+	public class PresenterInitializer : MonoBehaviour
+	{
+		[SerializeField] private UIFrame uiFrame;
+
+		private void Reset()
+		{
+			uiFrame = FindObjectOfType<UIFrame>();
+		}
+
+		private void Awake()
+		{
+			if (uiFrame == null)
+			{
+				uiFrame = FindObjectOfType<UIFrame>();
+			}
+		}
+
+		private void Start()
+		{
+			if (uiFrame)
+			{
+				BasePresenter<IScreenController>[] childPresenters = GetComponentsInChildren<BasePresenter<IScreenController>>(true);
+
+				foreach (var presenter in childPresenters)
+				{
+					presenter.Initialize(uiFrame);
+				}
+			}
+		}
+	}
+}
