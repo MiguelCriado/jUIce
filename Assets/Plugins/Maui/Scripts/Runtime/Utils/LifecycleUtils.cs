9fff47e25c6264851c4d665574ff7cc9879ed233
Miguel Criado <macro.masek@gmail.com>
2020-08-09 19:48:26 +0200

    Add LifecycleUtils

---
+++ Assets/Plugins/Maui/Scripts/Runtime/Utils/LifecycleUtils.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace Maui
+{
+	public class LifecycleUtils : MonoBehaviour
+	{
+		public static event Action OnUpdate
+		{
+			add => Instance.onUpdate += value;
+			remove => Instance.onUpdate -= value;
+		}
+		
+		public static event Action OnLateUpdate
+		{
+			add => Instance.onLateUpdate += value;
+			remove => Instance.onLateUpdate -= value;
+		}
+
+		private event Action onUpdate;
+		private event Action onLateUpdate;
+		
+		private static LifecycleUtils Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					GameObject gameObject = new GameObject("LifecycleUtils");
+					instance = gameObject.AddComponent<LifecycleUtils>();
+					DontDestroyOnLoad(gameObject);
+				}
+
+				return instance;
+			}
+		}
+		
+		private static LifecycleUtils instance;
+
+		private void Start()
+		{
+			if (instance != this)
+			{
+				Destroy(gameObject);
+			}
+		}
+
+		private void Update()
+		{
+			onUpdate?.Invoke();
+		}
+
+		private void LateUpdate()
+		{
+			onLateUpdate?.Invoke();
+		}
+	}
+}
