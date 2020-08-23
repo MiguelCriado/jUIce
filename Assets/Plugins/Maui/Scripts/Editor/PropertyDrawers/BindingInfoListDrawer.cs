d4c2144784add2c8e88baf704596beda535613db
Miguel Criado <macro.masek@gmail.com>
2020-08-23 18:19:59 +0200

    Rename BindingList as BindingInfoList

--- Assets/Plugins/Maui/Scripts/Editor/PropertyDrawers/BindingInfoListDrawer.cs
+++ Assets/Plugins/Maui/Scripts/Editor/PropertyDrawers/BindingInfoListDrawer.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 namespace Maui.Editor
 {
-	[CustomPropertyDrawer(typeof(BindingList))]
-	public class BindingListDrawer : PropertyDrawer
+	[CustomPropertyDrawer(typeof(BindingInfoList))]
+	public class BindingInfoListDrawer : PropertyDrawer
 	{
+		private static readonly float ElementMargin = EditorGUIUtility.standardVerticalSpacing;
+
 		private bool isCached;
-		private BindingList target;
+		private BindingInfoList target;
 		private SerializedProperty listProperty;
 		private ReorderableList reorderableList;
 
@@ -33,8 +35,8 @@ namespace Maui.Editor
 		{
 			if (isCached == false)
 			{
-				target = serializedProperty.GetValue<BindingList>();
-				listProperty = serializedProperty.FindPropertyRelative("bindings");
+				target = serializedProperty.GetValue<BindingInfoList>();
+				listProperty = serializedProperty.FindPropertyRelative("bindingInfoList");
 				reorderableList = GetList(listProperty);
 				isCached = true;
 			}
@@ -45,6 +47,7 @@ namespace Maui.Editor
 			if ( reorderableList == null )
 			{
 				reorderableList = new ReorderableList( serializedProperty.serializedObject, serializedProperty );
+				reorderableList.drawHeaderCallback += DrawHeaderCallback;
 				reorderableList.elementHeightCallback += ElementHeightCallback;
 				reorderableList.drawElementCallback += DrawElementCallback;
 				reorderableList.onAddCallback += OnAddCallback;
@@ -53,15 +56,23 @@ namespace Maui.Editor
 			return reorderableList;
 		}
 
+		private void DrawHeaderCallback(Rect rect)
+		{
+			EditorGUI.LabelField(rect, "Binding Info List");
+		}
+
 		private float ElementHeightCallback(int index)
 		{ 
 			SerializedProperty element = reorderableList.serializedProperty.GetArrayElementAtIndex(index);
-			return EditorGUI.GetPropertyHeight(element);
+			return EditorGUI.GetPropertyHeight(element) + ElementMargin;
 		}
 
 		private void DrawElementCallback(Rect rect, int index, bool isActive, bool isFocused)
 		{
 			SerializedProperty element = reorderableList.serializedProperty.GetArrayElementAtIndex(index);
+			rect.y += ElementMargin * 0.5f;
+			rect.height -= ElementMargin * 0.5f;
+			
 			EditorGUI.PropertyField(rect, element, GUIContent.none);
 		}
 		
