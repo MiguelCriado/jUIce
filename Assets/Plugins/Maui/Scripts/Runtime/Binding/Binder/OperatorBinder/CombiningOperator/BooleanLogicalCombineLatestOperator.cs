42719dddac25980cb6974dec3d9109f0b8303249
Miguel Criado <macro.masek@gmail.com>
2020-08-26 21:28:14 +0200

    Add CombiningOperator

---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/CombiningOperator/BooleanLogicalCombineLatestOperator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maui
+{
+	public class BooleanLogicalCombineLatestOperator : CombiningOperator<bool, bool>
+	{
+		public enum LogicalOperation
+		{
+			AND,
+			OR
+		}
+
+		[SerializeField] private LogicalOperation operation;
+
+		protected override bool Combine(int triggerIndex, IReadOnlyList<bool> list)
+		{
+			bool result = list[0];
+			int i = 1;
+
+			while (i < list.Count && KeepEvaluating(result))
+			{
+				result = Evaluate(result, list[i]);
+				i++;
+			}
+
+			return result;
+		}
+
+		private bool Evaluate(bool a, bool b)
+		{
+			switch (operation)
+			{
+				default:
+				case LogicalOperation.AND: return a && b;
+				case LogicalOperation.OR: return a || b;
+			}
+		}
+
+		private bool KeepEvaluating(bool currentValue)
+		{
+			switch (operation)
+			{
+				default:
+				case LogicalOperation.AND: return currentValue == true;
+				case LogicalOperation.OR: return currentValue == false;
+			}
+		}
+	}
+}
---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/CombiningOperator/CombiningOperator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Maui
+{
+	public abstract class CombiningOperator<TFrom, TTo> : ViewModelComponent, IViewModelInjector
+	{
+		public Type InjectionType => typeof(OperatorBinderVariableViewModel<TTo>);
+
+		public ViewModelComponent Target => this;
+
+		[SerializeField] private BindingInfoList fromBinding = new BindingInfoList(typeof(IReadOnlyObservableVariable<TFrom>));
+
+		private BindingList<TFrom> bindingList;
+		private ObservableVariable<TTo> exposedProperty;
+
+		protected virtual void Awake()
+		{
+			exposedProperty = new ObservableVariable<TTo>();
+			ViewModel = new OperatorBinderVariableViewModel<TTo>(exposedProperty);
+			
+			bindingList = new BindingList<TFrom>(this, fromBinding);
+			bindingList.VariableChanged += BindingListVariableChangedHandler;
+		}
+
+		protected virtual void OnEnable()
+		{
+			bindingList.Bind();
+		}
+
+		protected virtual void OnDisable()
+		{
+			bindingList.Unbind();
+		}
+
+		protected abstract TTo Combine(int triggerIndex, IReadOnlyList<TFrom> list);
+
+		private void BindingListVariableChangedHandler(int index, TFrom newValue)
+		{
+			exposedProperty.Value = Combine(index, bindingList.Values);
+		}
+	}
+}
