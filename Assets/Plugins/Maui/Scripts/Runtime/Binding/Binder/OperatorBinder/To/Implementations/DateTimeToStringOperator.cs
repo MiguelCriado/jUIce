62ccf4f0f7ef7941153e1093fdf25a5e13c4e02a
Miguel Criado <macro.masek@gmail.com>
2020-09-16 19:34:47 +0200

    Add a couple of ToString operators for some common types

---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/To/Implementations/DateTimeToStringOperator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Maui
+{
+	public class DateTimeToStringOperator : ToOperator<DateTime, string>
+	{
+		protected override string Convert(DateTime value)
+		{
+			return value.ToString(CultureInfo.CurrentCulture);
+		}
+	}
+}
---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/To/Implementations/Vector2ToStringOperator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Maui
+{
+	public class Vector2ToStringOperator : ToOperator<Vector2, string>
+	{
+		protected override string Convert(Vector2 value)
+		{
+			return value.ToString();
+		}
+	}
+}
