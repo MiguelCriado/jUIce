12324cf50b4658295dbf45016cdb9e14a0672838
Miguel Criado <macro.masek@gmail.com>
2020-09-20 14:05:43 +0200

    Add a couple of Unity Binders

---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/Modules/Unity/ActivateGameObjectBinder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Maui
+{
+	public class ActivateGameObjectBinder : VariableBinder<bool>
+	{
+		[SerializeField] private GameObject target;
+
+		protected override void Refresh(bool value)
+		{
+			if (target)
+			{
+				target.SetActive(value);
+			}
+		}
+	}
+}
