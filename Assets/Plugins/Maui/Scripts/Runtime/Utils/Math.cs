a5bed3f47a5e4b16fcc824479f970b87273f3761
Miguel Criado <macro.masek@gmail.com>
2020-10-12 19:55:58 +0200

    Add Remap function

---
+++ Assets/Plugins/Maui/Scripts/Runtime/Utils/Math.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Maui.Utils
+{
+	public static class Math
+	{
+		public static float Remap(float iMin, float iMax, float oMin, float oMax, float value)
+		{
+			float t = Mathf.InverseLerp(iMin, iMax, value);
+			return Mathf.Lerp(oMin, oMax, t);
+		}
+	}
+}
