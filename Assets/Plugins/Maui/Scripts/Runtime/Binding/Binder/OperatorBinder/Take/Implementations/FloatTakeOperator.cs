ce7b7bb25535a066e90752afb8c2feb223cb0523
Miguel Criado <macro.masek@gmail.com>
2020-11-06 18:53:04 +0100

    Add Take operator

---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/Take/Implementations/FloatTakeOperator.cs
@@ -0,0 +1,7 @@
+﻿namespace Maui
+{
+	public class FloatTakeOperator : TakeOperator<float>
+	{
+		
+	}
+}
---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/Take/TakeCommandBindingProcessor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Maui
+{
+	public class TakeCommandBindingProcessor<T> : CommandBindingProcessor<T, T>
+	{
+		private readonly int takeAmount;
+		private int valueReceivedCount;
+		
+		public TakeCommandBindingProcessor(BindingInfo bindingInfo, Component context, int takeAmount)
+			: base(bindingInfo, context)
+		{
+			this.takeAmount = takeAmount;
+			valueReceivedCount = 0;
+		}
+
+		public override void Bind()
+		{
+			valueReceivedCount = 0;
+			
+			base.Bind();
+		}
+
+		protected override void ProcessedCommandExecuteRequestedHandler(T parameter)
+		{
+			valueReceivedCount++;
+			
+			if (valueReceivedCount <= takeAmount)
+			{
+				base.ProcessedCommandExecuteRequestedHandler(parameter);
+			}
+		}
+		
+		protected override T ProcessValue(T value)
+		{
+			return value;
+		}
+	}
+}
