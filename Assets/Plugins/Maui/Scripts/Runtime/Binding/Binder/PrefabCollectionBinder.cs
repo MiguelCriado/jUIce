5b1bde5367b73a64e849572403519635b0b57b29
Miguel Criado <macro.masek@gmail.com>
2020-11-11 21:14:26 +0100

    Move PrefabPicker out of PrefabCollectionBinder

--- Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/PrefabCollectionBinder.cs
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/PrefabCollectionBinder.cs
@@ -7,20 +7,29 @@ namespace Maui
 	public class PrefabCollectionBinder : CollectionBinder<object>
 	{
 		public IReadOnlyList<CollectionItemViewModelComponent> CurrentItems => currentItems;
-		
-		[SerializeField] private List<CollectionItemViewModelComponent> prefabs;
 
-		private Transform container;
+		[SerializeField] private List<CollectionItemViewModelComponent> prefabs;
+		[SerializeField] private Transform itemsContainer;
+		
+		private Transform Container => itemsContainer ? itemsContainer : transform;
+		
+		private PrefabPicker<CollectionItemViewModelComponent> prefabPicker;
 		private List<CollectionItemViewModelComponent> currentItems;
-		private Dictionary<Type, CollectionItemViewModelComponent> prefabResolutionCache;
+
+		protected virtual void OnValidate()
+		{
+			if (itemsContainer == null)
+			{
+				itemsContainer = transform;
+			}
+		}
 
 		protected override void Awake()
 		{
 			base.Awake();
 
-			container = transform;
+			prefabPicker = new PrefabPicker<CollectionItemViewModelComponent>(prefabs);
 			currentItems = new List<CollectionItemViewModelComponent>();
-			prefabResolutionCache = new Dictionary<Type, CollectionItemViewModelComponent>();
 		}
 
 		protected override void OnCollectionReset()
@@ -50,7 +59,7 @@ namespace Maui
 
 		protected override void OnCollectionItemReplaced(int index, object oldValue, object newValue)
 		{
-			if (GetBestPrefabFromCache(oldValue) == GetBestPrefabFromCache(newValue))
+			if (prefabPicker.FindBestPrefab(oldValue) == prefabPicker.FindBestPrefab(newValue))
 			{
 				SetItemValue(index, newValue);
 			}
@@ -88,8 +97,8 @@ namespace Maui
 
 		private void InsertItem(int index, object value)
 		{
-			CollectionItemViewModelComponent bestPrefab = GetBestPrefabFromCache(value);
-			CollectionItemViewModelComponent newItem = SpawnItem(bestPrefab, container);
+			CollectionItemViewModelComponent bestPrefab = prefabPicker.FindBestPrefab(value);
+			CollectionItemViewModelComponent newItem = SpawnItem(bestPrefab, Container);
 			currentItems.Insert(index, newItem);
 			newItem.transform.SetSiblingIndex(index);
 			SetItemValue(index, value);
@@ -106,81 +115,6 @@ namespace Maui
 			currentItems.RemoveAt(oldIndex);
 			currentItems.Insert(newIndex, item);
 			item.transform.SetSiblingIndex(newIndex);
-		}
-
-		private CollectionItemViewModelComponent GetBestPrefabFromCache(object value)
-		{
-			Type valueType = value.GetType();
-
-			if (prefabResolutionCache.TryGetValue(valueType, out var result) == false)
-			{
-				result = FindBestPrefab(valueType);
-
-				if (result != null)
-				{
-					prefabResolutionCache[valueType] = result;
-				}
-			}
-
-			return result;
-		}
-
-		private CollectionItemViewModelComponent FindBestPrefab(Type valueType)
-		{
-			CollectionItemViewModelComponent result = null;
-			int bestDepth = -1;
-
-			foreach (CollectionItemViewModelComponent prefab in prefabs)
-			{
-				Type injectionType = prefab.InjectionType;
-				Type viewModelType;
-
-				if (injectionType != null
-				    && (viewModelType = GetViewModelType(injectionType)) != null
-				    && viewModelType.GenericTypeArguments[0].IsAssignableFrom(valueType))
-				{
-					Type dataType = viewModelType.GenericTypeArguments[0];
-					Type baseType = dataType.BaseType;
-					int depth = 0;
-
-					while (baseType != null)
-					{
-						depth++;
-						baseType = baseType.BaseType;
-					}
-
-					if (depth > bestDepth)
-					{
-						bestDepth = depth;
-						result = prefab;
-					}
-				}
-			}
-
-			return result;
-		}
-
-		private Type GetViewModelType(Type runtimeType)
-		{
-			Type result = null;
-
-			Type genericType = null;
-			
-			if (runtimeType.IsGenericType)
-			{
-				genericType = runtimeType.GetGenericTypeDefinition();
-			}
-
-			if (genericType != null && genericType == typeof(BindableViewModel<>))
-			{
-				result = runtimeType;
-			}
-			else if (runtimeType.BaseType != null)
-			{
-				result = GetViewModelType(runtimeType.BaseType);
-			}
-
-			return result;
 		}
 	}
 }
