9bdf0ade01776fbb2e1ea321848d231912320911
Miguel Criado <macro.masek@gmail.com>
2020-11-12 20:12:29 +0100

    Add common value-types for DelayOperator

---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/Delay/Implementations/DoubleDelayOperator.cs
@@ -0,0 +1,7 @@
+﻿namespace Maui
+{
+	public class DoubleDelayOperator : DelayOperator<double>
+	{
+		
+	}
+}
---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/Delay/Implementations/LongDelayOperator.cs
@@ -0,0 +1,7 @@
+﻿namespace Maui
+{
+	public class LongDelayOperator : DelayOperator<long>
+	{
+		
+	}
+}
