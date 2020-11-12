a76167424c03d1dbfe12b1e5e7935f971770806d
Miguel Criado <macro.masek@gmail.com>
2020-11-12 20:24:01 +0100

    Add common value-types for ScanOperator

---
+++ Assets/Plugins/Maui/Scripts/Runtime/Binding/Binder/OperatorBinder/Scan/Implementations/DoubleScanOperator.cs
@@ -0,0 +1,53 @@
+﻿using Maui.Scan;
+using UnityEngine;
+
+namespace Maui
+{
+	public class DoubleScanOperator : ScanOperator<double>
+	{
+		[SerializeField] private MathOperationType operation;
+
+		private int callCount;
+
+		protected override void OnEnable()
+		{
+			callCount = 0;
+			
+			base.OnEnable();
+		}
+
+		protected override double Scan(double value, double accumulatedValue)
+		{
+			double result = value;
+			
+			switch (operation)
+			{
+				case MathOperationType.Addition: result = accumulatedValue + value; break;
+				case MathOperationType.Substraction: result = accumulatedValue - value; break;
+				case MathOperationType.Multiplication: result = accumulatedValue * value; break;
+				case MathOperationType.Division: result = accumulatedValue / value; break;
+				case MathOperationType.Power: result = callCount == 0 ? value : Mathf.Pow((float)accumulatedValue, (float)value); break;
+			}
+
+			callCount++;
+
+			return result;
+		}
+
+		protected override double GetInitialAccumulatedValue()
+		{
+			double result = 0;
+			
+			switch (operation)
+			{
+				case MathOperationType.Addition: result = 0; break;
+				case MathOperationType.Substraction: result = 0; break;
+				case MathOperationType.Multiplication: result = 1; break;
+				case MathOperationType.Division: result = 1; break;
+				case MathOperationType.Power: result = 1; break;
+			}
+
+			return result;
+		}
+	}
+}
