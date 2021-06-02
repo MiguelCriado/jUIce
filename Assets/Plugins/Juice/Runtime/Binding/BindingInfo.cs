ec909783b2fac3a62962ff4827ee14a49e28d393
Miguel Criado <macro.masek@gmail.com>
2021-06-02 20:41:47 +0200

    Add shortcuts to create BindingInfo instances for observable types

--- Assets/Plugins/Juice/Runtime/Binding/BindingInfo.cs
+++ Assets/Plugins/Juice/Runtime/Binding/BindingInfo.cs
@@ -26,5 +26,35 @@ namespace Juice
 		{
 			type = new SerializableType(targetType);
 		}
+
+		public static BindingInfo Variable<T>()
+		{
+			return new BindingInfo(typeof(IReadOnlyObservableVariable<T>));
+		}
+
+		public static BindingInfo Collection<T>()
+		{
+			return new BindingInfo(typeof(IReadOnlyObservableCollection<T>));
+		}
+
+		public static BindingInfo Command()
+		{
+			return new BindingInfo(typeof(IObservableCommand));
+		}
+
+		public static BindingInfo Command<T>()
+		{
+			return new BindingInfo(typeof(IObservableCommand<T>));
+		}
+
+		public static BindingInfo Event()
+		{
+			return new BindingInfo(typeof(IObservableEvent));
+		}
+
+		public static BindingInfo Event<T>()
+		{
+			return new BindingInfo(typeof(IObservableEvent<T>));
+		}
 	}
 }
