3a243a72393eaf9b298dbfa281da406332d72351
Miguel Criado <macro.masek@gmail.com>
2021-08-19 21:12:09 +0200

    Support plain Behaviours in EnableBehaviourBinder

---
+++ Assets/Plugins/Juice/Runtime/Binding/Binder/Modules/Unity/EnableBehaviourBinder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Juice
+{
+	public class EnableBehaviourBinder : ComponentBinder
+	{
+		[SerializeField] private BindingInfo isEnabled = BindingInfo.Variable<bool>();
+		[SerializeField] private List<Behaviour> targets = default;
+
+		protected override void Awake()
+		{
+			base.Awake();
+
+			RegisterVariable<bool>(isEnabled).OnChanged(Refresh);
+		}
+
+		private void Refresh(bool value)
+		{
+			foreach (Behaviour current in targets)
+			{
+				if (current)
+				{
+					current.enabled = value;
+				}
+			}
+		}
+	}
+}
