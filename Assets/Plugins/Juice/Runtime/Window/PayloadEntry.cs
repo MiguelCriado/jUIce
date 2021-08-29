c04318babb046e48db3afb1386914672172540dc
Miguel Criado <macro.masek@gmail.com>
2021-08-29 18:18:25 +0000

    Add PayloadEntry

---
+++ Assets/Plugins/Juice/Runtime/Window/PayloadEntry.cs
@@ -0,0 +1,38 @@
+﻿namespace Juice
+{
+	public class PayloadEntry<T>
+	{
+		public bool Exists => window.GetFromPayload<T>(key, out _);
+
+		private readonly string key;
+		private readonly IWindow window;
+
+		public PayloadEntry(string key, IWindow window)
+		{
+			this.key = key;
+			this.window = window;
+		}
+
+		public T GetValue(T fallback = default)
+		{
+			if (window.GetFromPayload(key, out T result) == false)
+			{
+				result = fallback;
+			}
+
+			return result;
+		}
+
+		public void Remove()
+		{
+			window.RemoveFromPayload(key);
+		}
+
+		public T Consume(T fallback = default)
+		{
+			T result = GetValue(fallback);
+			Remove();
+			return result;
+		}
+	}
+}
